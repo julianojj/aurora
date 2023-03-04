29775804ebef6cd3b77dd4bf9f431c2d7d9df05d
Juliano Silva <cont.juliano@outlook.com>
2023-03-03 22:32:41 -0300

    test: update a test create artboard

--- internal/core/usecases/create_artboard_test.go
+++ internal/core/usecases/create_artboard_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldCreateDesign(t *testing.T) {
+func TestShouldCreateArtboard(t *testing.T) {
 	projectRepository := repository.NewProjectRepositoryMemory()
 	artboardRepository := repository.NewArtboardRepositoryMemory()
 	createProject := NewCreateProject(projectRepository)
