9b968206d36c2b57a7a36a12f169fdf42013ba3c
Juliano Silva <cont.juliano@outlook.com>
2023-03-05 09:19:19 -0300

    feat: added a upload_file usecases

--- internal/core/usecases/upload_file.go
+++ internal/core/usecases/upload_file.go
@@ -3,14 +3,17 @@ package usecases
 import (
 	"fmt"
 	"io"
+	"os"
 	"path"
 
 	"github.com/google/uuid"
 	"github.com/julianojj/aurora/internal/core/domain"
+	"github.com/julianojj/aurora/internal/infra/adapters"
 )
 
 type UploadFile struct {
 	fileRepository domain.FileRepository
+	bucket         adapters.Bucket
 }
 
 type UploadFileInput struct {
@@ -20,22 +23,23 @@ type UploadFileInput struct {
 	Reader   io.Reader `json:"reader"`
 }
 
-func NewUploadFile(fileRepository domain.FileRepository) *UploadFile {
+func NewUploadFile(fileRepository domain.FileRepository, bucket adapters.Bucket) *UploadFile {
 	return &UploadFile{
 		fileRepository,
+		bucket,
 	}
 }
 
 func (u *UploadFile) Execute(input UploadFileInput) error {
 	fileID := uuid.NewString()
-	bucketName := "aurora"
+	bucketName := os.Getenv("MINIO_BUCKET_NAME")
 	ext := path.Ext(input.Name)
 	newName := fmt.Sprintf("%s%s", fileID, ext)
 	file, err := domain.NewFile(
 		fileID,
 		input.Name,
 		input.Mimetype,
-		fmt.Sprintf("%s/%s", bucketName, newName),
+		fmt.Sprintf("/%s/%s", bucketName, newName),
 		input.Size,
 		input.Reader,
 	)
@@ -45,6 +49,10 @@ func (u *UploadFile) Execute(input UploadFileInput) error {
 	err = u.fileRepository.Save(file)
 	if err != nil {
 		return nil
+	}
+	err = u.bucket.PutObject(file)
+	if err != nil {
+		return err
 	}
 	return nil
 }
