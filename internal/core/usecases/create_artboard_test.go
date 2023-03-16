a7af0ae97694e86319425695701d7b15fc6de2a3
Juliano Silva <cont.juliano@outlook.com>
2023-03-16 18:35:49 -0300

    test: added new tests to create_artboard_test

--- internal/core/usecases/create_artboard_test.go
+++ internal/core/usecases/create_artboard_test.go
@@ -7,6 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestReturnErrorIfProjectNotFound(t *testing.T) {
+	projectRepository := repository.NewProjectRepositoryMemory()
+	artboardRepository := repository.NewArtboardRepositoryMemory()
+	createArtboard := NewCreateArtboard(projectRepository, artboardRepository)
+	inputCreateArtboard := CreateArtboardInput{
+		ProjectID: "1",
+		Name:      "My Artboard",
+	}
+	err := createArtboard.Execute(inputCreateArtboard)
+	assert.EqualError(t, err, "project not found")
+}
+
+func TestReturnErrorIfEmptyArtboardName(t *testing.T) {
+	projectRepository := repository.NewProjectRepositoryMemory()
+	artboardRepository := repository.NewArtboardRepositoryMemory()
+	createProject := NewCreateProject(projectRepository)
+	createArtboard := NewCreateArtboard(projectRepository, artboardRepository)
+	inputCreateProject := CreateProjectInput{
+		Name: "Untitled Project",
+	}
+	output, _ := createProject.Execute(inputCreateProject)
+	inputCreateArtboard := CreateArtboardInput{
+		ProjectID: output.ProjectID,
+		Name:      "",
+	}
+	err := createArtboard.Execute(inputCreateArtboard)
+	assert.EqualError(t, err, "Name cannot be empty")
+}
+
 func TestShouldCreateArtboard(t *testing.T) {
 	projectRepository := repository.NewProjectRepositoryMemory()
 	artboardRepository := repository.NewArtboardRepositoryMemory()
