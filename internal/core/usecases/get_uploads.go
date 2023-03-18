82f6cdad8d473f80908cf1ef57fe7f70e14a261f
Juliano Silva <cont.juliano@outlook.com>
2023-03-18 11:37:19 -0300

    feat: added get_uploads usecase

---
+++ internal/core/usecases/get_uploads.go
@@ -0,0 +1,39 @@
+package usecases
+
+import "github.com/julianojj/aurora/internal/core/domain"
+
+type GetUploads struct {
+	fileRepository domain.FileRepository
+}
+
+type GetUploadsOutput struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+	Path  string `json:"path"`
+}
+
+func NewGetUploads(
+	fileRepository domain.FileRepository,
+) *GetUploads {
+	return &GetUploads{
+		fileRepository,
+	}
+}
+
+func (gu *GetUploads) Execute() ([]*GetUploadsOutput, error) {
+	uploads, err := gu.fileRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	var output []*GetUploadsOutput
+	for _, upload := range uploads {
+		output = append(output, &GetUploadsOutput{
+			ID:   upload.FileID,
+			Name: upload.Name,
+			Size: upload.Size,
+			Path:  upload.Path,
+		})
+	}
+	return output, nil
+}
