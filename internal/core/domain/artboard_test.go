97757c32549f6ee894286746bded3e8deb7645db
Juliano Silva <cont.juliano@outlook.com>
2023-03-26 09:28:55 -0300

    test: refactored test names in artboard_test

--- internal/core/domain/artboard_test.go
+++ internal/core/domain/artboard_test.go
@@ -6,37 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNotShouldCreateArtboardIfEmptyArtboardID(t *testing.T) {
-	artboard, err := NewArtboard(
+func TestReturnErrorIfEmptyArtboardID(t *testing.T) {
+	_, err := NewArtboard(
 		"",
 		"1",
 		"My Artboard",
 	)
 	assert.EqualError(t, err, "Artboard ID cannot be empty")
-	assert.Nil(t, artboard)
 }
 
-func TestNotShouldCreateArtboardIfEmptyProjectID(t *testing.T) {
-	artboard, err := NewArtboard(
+func TestReturnErrorIfEmptyProjectID(t *testing.T) {
+	_, err := NewArtboard(
 		"1",
 		"",
 		"My Artboard",
 	)
 	assert.EqualError(t, err, "Project ID cannot be empty")
-	assert.Nil(t, artboard)
 }
 
-func TestNotShouldCreateArtboardIfEmptyName(t *testing.T) {
-	artboard, err := NewArtboard(
+func TestReturnErrorIfEmptyName(t *testing.T) {
+	_, err := NewArtboard(
 		"1",
 		"1",
 		"",
 	)
 	assert.EqualError(t, err, "Name cannot be empty")
-	assert.Nil(t, artboard)
 }
 
-func TestShouldCreateArtboard(t *testing.T) {
+func TestCreateArtboard(t *testing.T) {
 	size := &Size{
 		Width:  100,
 		Height: 100,
@@ -54,7 +51,7 @@ func TestShouldCreateArtboard(t *testing.T) {
 		StrokeWidth: 1,
 		Opacity:     100,
 	}
-	layer, err := NewLayer(
+	layer, _ := NewLayer(
 		"1",
 		"1",
 		"My Layer",
@@ -64,14 +61,12 @@ func TestShouldCreateArtboard(t *testing.T) {
 		rotation,
 		properties,
 	)
-	assert.NoError(t, err)
-	artboard, err := NewArtboard(
+	artboard, _ := NewArtboard(
 		"1",
 		"1",
 		"My Artboard",
 	)
 	artboard.AddLayer(layer)
-	assert.NoError(t, err)
 	assert.NotNil(t, artboard)
 	assert.Equal(t, "My Artboard", artboard.Name)
 	assert.Len(t, artboard.Layers, 1)
