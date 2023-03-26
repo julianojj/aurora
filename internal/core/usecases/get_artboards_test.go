4384b082c01ffa817dc1e07a3dfc4330c8b3d713
Juliano Silva <cont.juliano@outlook.com>
2023-03-26 10:49:58 -0300

    test: added get_artboards_test

---
+++ internal/core/usecases/get_artboards_test.go
@@ -0,0 +1,29 @@
+package usecases
+
+import (
+	"testing"
+
+	"github.com/julianojj/aurora/internal/infra/repository"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetArtboards(t *testing.T) {
+	projectRepository := repository.NewProjectRepositoryMemory()
+	artboardRepository := repository.NewArtboardRepositoryMemory()
+	createProject := NewCreateProject(projectRepository)
+	createArtboard := NewCreateArtboard(projectRepository, artboardRepository)
+	getArtboards := NewGetArtboards(artboardRepository)
+	inputCreateProject := CreateProjectInput{
+		Name: "Untitled Project",
+	}
+	outputCreateProject, _ := createProject.Execute(inputCreateProject)
+	inputCreateArtboard := CreateArtboardInput{
+		ProjectID: outputCreateProject.ProjectID,
+		Name:      "My Artboard",
+	}
+	createArtboard.Execute(inputCreateArtboard)
+	artboards, _ := getArtboards.Execute(outputCreateProject.ProjectID)
+	assert.Equal(t, 1, len(artboards))
+	assert.Equal(t, "My Artboard", artboards[0].Name)
+	assert.Len(t, artboards[0].Layers, 0)
+}
