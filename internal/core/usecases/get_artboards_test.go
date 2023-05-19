1a268ba7079da9aa0350aa07e66d68c79457d34e
Juliano Silva <cont.juliano@outlook.com>
2023-05-19 12:09:26 -0300

    refactor: refactored tests

--- internal/core/usecases/get_artboards_test.go
+++ internal/core/usecases/get_artboards_test.go
@@ -21,8 +21,9 @@ func TestGetArtboards(t *testing.T) {
 		ProjectID: outputCreateProject.ProjectID,
 		Name:      "My Artboard",
 	}
-	createArtboard.Execute(inputCreateArtboard)
+	err := createArtboard.Execute(inputCreateArtboard)
 	artboards, _ := getArtboards.Execute(outputCreateProject.ProjectID)
+	assert.NoError(t, err)
 	assert.Equal(t, 1, len(artboards))
 	assert.Equal(t, "My Artboard", artboards[0].Name)
 }
