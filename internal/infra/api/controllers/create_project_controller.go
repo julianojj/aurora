cfe2b00f7d308d73cebcd29801be8ab399b8f852
Juliano Silva <cont.juliano@outlook.com>
2023-06-23 17:35:45 -0300

    refactor: refactored create project controller

--- internal/infra/api/controllers/create_project_controller.go
+++ internal/infra/api/controllers/create_project_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/julianojj/aurora/internal/core/exceptions"
 	"github.com/julianojj/aurora/internal/core/usecases"
 )
 
@@ -20,14 +21,33 @@ func NewCreateProjectController(createProject *usecases.CreateProject) *CreatePr
 func (cpc *CreateProjectController) Handle(c *gin.Context) {
 	var input usecases.CreateProjectInput
 	err := c.BindJSON(&input)
-	if err!= nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"code":    http.StatusBadRequest,
+		})
+		return
+	}
 	project, err := cpc.createProject.Execute(input)
-	if err!= nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-	c.JSON(http.StatusCreated, project)
+	if err == nil {
+		c.JSON(http.StatusCreated, project)
+		return
+	}
+	switch err.(type) {
+	case *exceptions.NotFoundException:
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": err.Error(),
+			"code":    http.StatusNotFound,
+		})
+	case *exceptions.ValidationException:
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"message": err.Error(),
+			"code":    http.StatusUnprocessableEntity,
+		})
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "internal server error",
+			"code":    http.StatusInternalServerError,
+		})
+	}
 }
