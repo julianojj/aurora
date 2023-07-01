9a5f3b7e09a13e901c2164d8a7423b975b635858
Juliano Silva <cont.juliano@outlook.com>
2023-06-30 21:39:06 -0300

    feat: added find method

--- internal/core/domain/file_repository.go
+++ internal/core/domain/file_repository.go
@@ -3,5 +3,6 @@ package domain
 type FileRepository interface {
 	Save(file *File) error
 	FindAll() ([]*File, error)
+	Find(fileId string) (*File, error)
 	Delete(fileID string) error
 }
--- internal/infra/repository/file_repository_memory.go
+++ internal/infra/repository/file_repository_memory.go
@@ -23,6 +23,15 @@ func (frm *FileRepositoryMemory) FindAll() ([]*domain.File, error) {
 	return frm.Files, nil
 }
 
+func (frm *FileRepositoryMemory) Find(fileID string) (*domain.File, error) {
+	for _, file := range frm.Files {
+		if file.FileID == fileID {
+			return file, nil
+		}
+	}
+	return nil, nil
+}
+
 func (frm *FileRepositoryMemory) Delete(fileID string) error {
 	var files []*domain.File
 	for _, file := range frm.Files {
