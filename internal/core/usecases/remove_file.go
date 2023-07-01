77138078d9a8b556aa380655c2ae5b62299f715d
Juliano Silva <cont.juliano@outlook.com>
2023-06-30 21:40:25 -0300

    refactor: refactored remove file to bucket

--- internal/core/usecases/remove_file.go
+++ internal/core/usecases/remove_file.go
@@ -1,19 +1,38 @@
 package usecases
 
-import "github.com/julianojj/aurora/internal/core/domain"
+import (
+	"github.com/julianojj/aurora/internal/core/domain"
+	"github.com/julianojj/aurora/internal/infra/adapters"
+)
 
 type RemoveFile struct {
 	fileRepository domain.FileRepository
+	bucket         adapters.Bucket
 }
 
-func NewRemoveFile(fileRepository domain.FileRepository) *RemoveFile {
+func NewRemoveFile(
+	fileRepository domain.FileRepository,
+	bucket adapters.Bucket,
+) *RemoveFile {
 	return &RemoveFile{
 		fileRepository,
+		bucket,
 	}
 }
 
 func (rf *RemoveFile) Execute(fileID string) error {
-	err := rf.fileRepository.Delete(fileID)
+	existingFile, err := rf.fileRepository.Find(fileID)
+	if err != nil {
+		return err
+	}
+	if existingFile == nil {
+		return nil
+	}
+	err = rf.bucket.DeleteObject(existingFile.Name)
+	if err != nil {
+		return err
+	}
+	err = rf.fileRepository.Delete(fileID)
 	if err != nil {
 		return err
 	}
