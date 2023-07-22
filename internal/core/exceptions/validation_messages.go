bb79164b474b166a68aaeaafaf7161a53d82e42c
Juliano Silva <cont.juliano@outlook.com>
2023-07-22 12:22:32 -0300

    refactor: refactored artboard domain

--- internal/core/exceptions/validation_messages.go
+++ internal/core/exceptions/validation_messages.go
@@ -2,3 +2,5 @@ package exceptions
 
 const EMPTY_PROJECT_ID = "Project ID cannot be empty"
 const EMPTY_PROJECT_NAME = "Project name cannot be empty"
+const EMPTY_ARTBOARD_ID = "Artboard ID cannot be empty"
+const EMPTY_ARTBOARD_NAME = "Artboard name cannot be empty"
