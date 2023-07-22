a9bbb18aeadd916aae3d01907bbb4a01576076d3
Juliano Silva <cont.juliano@outlook.com>
2023-07-22 11:42:04 -0300

    refactor: refactored project domain

--- internal/core/domain/project_test.go
+++ internal/core/domain/project_test.go
@@ -4,28 +4,28 @@ import (
 	"testing"
 	"time"
 
+	"github.com/julianojj/aurora/internal/core/exceptions"
 	"github.com/stretchr/testify/assert"
 )
 
 const ProjectName = "My Project"
 
-func TestNotShouldCreateProjectIfEmptyProjectID(t *testing.T) {
+func TestProject(t *testing.T) {
 	today := time.Now()
-	_, err := NewProject("", ProjectName, today, today)
-	assert.EqualError(t, err, "Project ID cannot be empty")
-}
+	project, _ := NewProject("1", "My Project", today, today)
 
-func TestNotShouldCreateProjectIfEmptyName(t *testing.T) {
-	today := time.Now()
-	_, err := NewProject("1", "", today, today)
-	assert.EqualError(t, err, "Name cannot be empty")
-}
-
-func TestShouldCreateProject(t *testing.T) {
-	today := time.Now()
-	project, _ := NewProject("1", ProjectName, today, today)
 	assert.Equal(t, "1", project.ID)
-	assert.Equal(t, ProjectName, project.Name)
+	assert.Equal(t, "My Project", project.Name)
 	assert.Equal(t, today, project.CreatedAt)
 	assert.Equal(t, today, project.UpdatedAt)
+
+	t.Run("Return exception if empty project id", func(t *testing.T) {
+		_, err := NewProject("", "My Project", today, today)
+		assert.EqualError(t, err, exceptions.EMPTY_PROJECT_ID)
+	})
+
+	t.Run("Return exception if empty project name", func(t *testing.T) {
+		_, err := NewProject("1", "", today, today)
+		assert.EqualError(t, err, exceptions.EMPTY_PROJECT_NAME)
+	})
 }
