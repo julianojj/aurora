0d15619e22a6dbfdfc9ce41501231f5aa80f77cd
Juliano Silva <cont.juliano@outlook.com>
2023-11-14 11:37:51 -0300

    feat: added env config

---
+++ internal/config/config_test.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	t.Run("Load config", func(t *testing.T) {
+		config := LoadConfig("../../.env")
+		assert.Equal(t, "http://localhost:4566", config.AWS_S3_ENDPOINT)
+		assert.Equal(t, "us-east-1", config.AWS_REGION)
+		assert.Equal(t, "access", config.AWS_ROOT_USER)
+		assert.Equal(t, "secretkey", config.AWS_ROOT_PASSWORD)
+		assert.Equal(t, "aurora", config.AWS_BUCKET_NAME)
+	})
+
+	t.Run("Return exception if try load config", func(t *testing.T) {
+		assert.PanicsWithValue(t, "error to load config", func() {
+			LoadConfig("./env")
+		})
+	})
+}
